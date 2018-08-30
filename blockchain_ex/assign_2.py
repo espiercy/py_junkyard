c2a1065fb841d16c5a5b9d8aecc0fd5792fc3ddf
Evan <evanpiercy@gmail.com>
2018-08-29 19:31:34 -0700

    assign_2

---
+++ blockchain_ex/assign_2.py
@@ -0,0 +1,13 @@
+names = ["Evan", "Kellee", "David", "Thao", "Leslie", "Brett", "Emery", "Ooglethorpe"]
+
+for name in names:
+    if len(name) > 5:
+        print(name)
+    if "n" in name or "N" in name:
+        print(name)
+
+while len(names) > 0:
+    print(names[-1])
+    names.pop()
+
+print("names all popped!")
